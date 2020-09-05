06d08450f17caf8d71d058d5421166aa7ebc8d5d
David <54184514+davidelp68@users.noreply.github.com>
2020-09-05 18:23:34 +0200

    Update MapsActivity.kt

--- app/src/main/java/com/dm/tutorialgooglemapsactivity401/MapsActivity.kt
+++ app/src/main/java/com/dm/tutorialgooglemapsactivity401/MapsActivity.kt
@@ -17,7 +17,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Ottieni SupportMapFragment e ricevi una notifica quando la mappa è pronta per essere utilizzata.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
